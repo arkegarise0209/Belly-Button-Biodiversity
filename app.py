09450f000c34ed66841ff1e87f35ae9b250d0938
Kegarise <kegarise@queensgranthigh.org>
2018-06-04 23:17:11 -0400

    pushed partial flask app

--- app.py
+++ app.py
@@ -15,6 +15,74 @@ Base.prepare(engine, reflect=True)
 session = Session(engine)
 
 # Create variables for tables in database
-Otu = Base.classes.otu
+OTU = Base.classes.otu
 Samples = Base.classes.samples
 Samples_Metadata = Base.classes.samples_metadata
+
+# Return the dashboard homepage
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Return a list of sample names
+@app.route("/names")
+def names():
+
+    # Empty list for sample ids
+    sample_ids = []
+    
+    # Grab metadata from table
+    results = session.query(Samples_Metadata.SAMPLEID)
+
+    # Loop through query & grab ids
+    for result in results:
+        sample_ids.append("BB_" + str(result[0]))
+
+    return jsonify(sample_ids)
+
+# Return a list of OTU descriptions 
+@app.route("/otu")
+def otu():
+
+    # Empty list for OTU descriptions
+    OTU_desc = []
+
+    # Grab metadata from table
+    results = session.query(OTU.lowest_taxonomic_unit_found)
+
+    # Loop through query and grab descriptions
+    for result in results:
+        OTU_desc.append(result[0])
+    
+    return jsonify(OTU_desc)
+
+# Returns a json dictionary of sample metadata
+@app.route("/metadata/<sample>")
+def metadata(sample):
+    
+    # Grab input
+    sample_id = sample.replace("BB_", "")
+
+    # Empty dictionary for data
+    sample_metadata = {}
+
+    # Grab metadata from table
+    database = session.query(Samples_Metadata)
+
+    # Loop through query & grab data
+    for data in database:
+        if (sample_id == data.SAMPLEID):
+            sample_metadata["AGE"] = data.AGE
+            sample_metadata["BBTYPE"] = data.BBTYPE
+            sample_metadata["ETHNICITY"] = data.ETHNICITY
+            sample_metadata["GENDER"] = data.GENDER
+            sample_metadata["LOCATION"] = data.LOCATION
+            sample_metadata["SAMPLEID"] = data.SAMPLEID
+
+    return jsonify(sample_metadata)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
